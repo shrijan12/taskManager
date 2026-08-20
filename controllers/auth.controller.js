2c38a94ca592291e726b1e5e9e810e467a4b6969
shrijan12 <shrinandhungel3@gmail.com>
2026-08-20 09:00:05 +0545

    done small portion of reg controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,66 @@
+import User from "../models/User.model.js";
+import bcrypt from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
+
 export const registerController = async (req, res) => {
-  res.status(200).json({ message: "Register route" });
+  try {
+    //destructinng the body
+    const { name, email, password } = req.body;
+    // generic validation
+    if (!name || !email || !password) {
+      return res.status(400).json({
+        success: "false",
+        message: "Please fill all the required fields",
+      });
+    }
+    //password length validation
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: "false",
+        message: "Password must be least 6 characters long",
+      });
+    }
+    //check if user already exists
+
+    const existingUser = await User.findOne({ email: email.toLowerCase() });
+    //if does, return error
+    if (existingUser) {
+      return res.status(400).json({
+        success: "false",
+        message: "user already exists with the provided email",
+      });
+    }
+    //lets hash the password now
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //now with the hashed password lets create the new user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    //generate token and send it with the response
+    const token = generateToken(user._id);
+
+    //return success response now,
+    res.status(201).json({
+      success: "true",
+      message: "Registration Successfull",
+      data: {
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+          role: user.role,
+        },
+        token,
+      },
+    });
+  } catch (error) {
+    console.log("error while registering the user", error.message);
+    next(error);
+  }
 };
 
 export const loginController = async (req, res) => {
